cc2b93f7381c25cb9b539ecbc9babff138b9bca7
unknown <croudebush15@gmail.com>
2019-10-16 22:03:02 -0500

    actualizar stored procedures

--- eSport-ProcAlm.sql
+++ eSport-ProcAlm.sql
@@ -7,18 +7,25 @@ CREATE PROCEDURE ListarPersonal AS
 SELECT * FROM PERSONAL
 GO
 
-EXEC ListarPersonal
-GO
+--EXEC ListarPersonal
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarPersonal
-@IdPersonal INT, @ApePersonal VARCHAR(30), @NomPersonal varchar(30), @PaisPersonal varchar(30), @CargoPersonal varchar(30)
+@ApePersonal VARCHAR(30), @NomPersonal varchar(30), @PaisPersonal varchar(30), @CargoPersonal varchar(30)
 AS
+declare @IdPersonal INT
+set @IdPersonal=(Select count(*) from PERSONAL)
+if @IdPersonal=0 
+       set @IdPersonal = 100
+else
+        set @IdPersonal=(Select (Max (IdPersonal)+ 1)
+    From PERSONAL)
 Insert into PERSONAL values (@IdPersonal, @ApePersonal,@NomPersonal,@PaisPersonal,@CargoPersonal)
 go
 
-EXEC InsertarPersonal @IdPersonal = 1, @APEPERSONAL = 'FASDF', @NOMPERSONAL = 'FDSA', @PAISPERSONAL = 'ASDF', @CARGOPERSONAL = 'ASDFF'
-GO
+--EXEC InsertarPersonal @IdPersonal = 1, @APEPERSONAL = 'FASDF', @NOMPERSONAL = 'FDSA', @PAISPERSONAL = 'ASDF', @CARGOPERSONAL = 'ASDFF'
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarPersonal
@@ -27,8 +34,8 @@ AS
 DELETE FROM PERSONAL WHERE IdPersonal = @IdPersonal
 GO
 
-EXEC EliminarPersonal @IDPERSONAL = 1
-GO
+--EXEC EliminarPersonal @IDPERSONAL = 1
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarPersonal
@@ -37,8 +44,8 @@ AS
 SELECT * FROM PERSONAL WHERE IdPersonal = @IdPersonal
 GO
 
-EXEC ConsultarPersonal @IDPERSONAL = 1
-GO
+--EXEC ConsultarPersonal @IDPERSONAL = 1
+--GO
 
 --Actualizar
 CREATE PROCEDURE ActualizarPersonal
@@ -54,8 +61,8 @@ BEGIN
     WHERE IdPersonal=@IdPersonal
 END
 
-EXEC ActualizarPersonal @IDPERSONAL = 1, @PaisPersonal = 'USA'
-GO
+--EXEC ActualizarPersonal @IDPERSONAL = 1, @PaisPersonal = 'USA'
+--GO
 
 --2. Torneo
 --Listar
@@ -63,18 +70,25 @@ CREATE PROCEDURE ListarTorneo AS
 SELECT * FROM TORNEO
 GO
 
-EXEC ListarTorneo
-GO
+--EXEC ListarTorneo
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarTorneo
-@IdTorneo int, @NomTorneo varchar(30), @OrgTorneo varchar(30), @PaisTorneo varchar(30), @LugarTorneo varchar(30), @FechaInicioTorneo datetime, @FechaFinTorneo datetime, @IdJuego int
+@NomTorneo varchar(30), @OrgTorneo varchar(30), @PaisTorneo varchar(30), @LugarTorneo varchar(30), @FechaInicioTorneo datetime, @FechaFinTorneo datetime, @IdJuego int
 AS
+declare @IdTorneo int
+set @IdTorneo=(Select count(*) from TORNEO)
+if @IdTorneo=0 
+       set @IdTorneo = 10
+else
+        set @IdTorneo=(Select (Max (IdTorneo)+ 10)
+    From TORNEO)
 Insert into TORNEO values (@IdTorneo, @NomTorneo,@OrgTorneo,@PaisTorneo,@LugarTorneo,@FechaInicioTorneo,@FechaFinTorneo,@IdJuego)
 go
 
-EXEC InsertarTorneo @IdTorneo = 1, @NomTorneo = 'test', @OrgTorneo = 'test', @PaisTorneo = 'test', @LugarTorneo = 'test', @FechaInicioTorneo = '20180815', @FechaFinTorneo = '20180815', @IdJuego = 1
-GO
+--EXEC InsertarTorneo @IdTorneo = 1, @NomTorneo = 'test', @OrgTorneo = 'test', @PaisTorneo = 'test', @LugarTorneo = 'test', @FechaInicioTorneo = '20180815', @FechaFinTorneo = '20180815', @IdJuego = 1
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarTorneo
@@ -83,8 +97,8 @@ AS
 DELETE FROM TORNEO WHERE IdTorneo = @IdTorneo
 GO
 
-EXEC EliminarTorneo @IDTORNEO = 1
-GO
+--EXEC EliminarTorneo @IDTORNEO = 1
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarTorneo
@@ -93,8 +107,8 @@ AS
 SELECT * FROM TORNEO WHERE IdTorneo = @IdTorneo
 GO
 
-EXEC ConsultarTorneo @idtorneo = 1
-GO
+--EXEC ConsultarTorneo @idtorneo = 1
+--GO
 
 --Actualizar 
 CREATE PROCEDURE ActualizarTorneo
@@ -113,8 +127,8 @@ BEGIN
     WHERE IdTorneo=@IdTorneo
 END
 
-EXEC ActualizarTorneo @IDTORNEO = 1, @LUGARTORNEO = 'USA'
-GO
+--EXEC ActualizarTorneo @IDTORNEO = 1, @LUGARTORNEO = 'USA'
+--GO
 
 --3. Juego
 --Listar
@@ -122,18 +136,25 @@ CREATE PROCEDURE ListarJuego AS
 SELECT * FROM JUEGO
 GO
 
-EXEC ListarJuego
-GO
+--EXEC ListarJuego
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarJuego
-@IdJuego int, @NomJuego varchar(30), @DesaJuego varchar(30), @FechaJuego datetime, @GeneJuego varchar(20)
+@NomJuego varchar(30), @DesaJuego varchar(30), @FechaJuego datetime, @GeneJuego varchar(20)
 AS
+declare @IdJuego int
+set @IdJuego=(Select count(*) from JUEGO)
+if @IdJuego=0 
+       set @IdJuego = 1
+else
+        set @IdJuego=(Select (Max (IdJuego)+ 1)
+    From JUEGO)
 Insert into JUEGO values (@IdJuego,@NomJuego,@DesaJuego,@FechaJuego,@GeneJuego)
 go
 
-EXEC InsertarJuego @idjuego = 10, @nomjuego = 'test', @desajuego = 'test', @fechajuego= '20180815', @genejuego = 'test'
-GO
+--EXEC InsertarJuego @idjuego = 10, @nomjuego = 'test', @desajuego = 'test', @fechajuego= '20180815', @genejuego = 'test'
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarJuego
@@ -142,8 +163,8 @@ AS
 DELETE FROM JUEGO WHERE IdJuego = @IdJuego
 GO
 
-EXEC EliminarJuego @IDJUEGO = 10
-GO
+--EXEC EliminarJuego @IDJUEGO = 10
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarJuego
@@ -152,8 +173,8 @@ AS
 SELECT * FROM JUEGO WHERE IdJuego = @IdJuego
 GO
 
-EXEC ConsultarJuego  @idjuego = 10
-GO
+--EXEC ConsultarJuego  @idjuego = 10
+--GO
 
 --Actualizar 
 CREATE PROCEDURE ActualizarJuego
@@ -170,8 +191,8 @@ BEGIN
     WHERE IdJuego=@IdJuego
 END
 
-EXEC ActualizarJuego @idjuego = 10, @nomjuego = 'game'
-GO
+--EXEC ActualizarJuego @idjuego = 10, @nomjuego = 'game'
+--GO
 
 --4. Equipo
 --Listar
@@ -179,18 +200,24 @@ CREATE PROCEDURE ListarEquipo AS
 SELECT * FROM EQUIPO
 GO
 
-EXEC ListarEquipo
-GO
+--EXEC ListarEquipo
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarEquipo
-@IdEquipo int, @NomEquipo varchar(30), @PaisEquipo varchar(30)
+@NomEquipo varchar(30), @PaisEquipo varchar(30)
 AS
+declare @IdEquipo int
+set @IdEquipo=(Select count(*) from EQUIPO)
+if @IdEquipo=0 
+       set @IdEquipo = 1000
+else
+        set @IdEquipo=(Select (Max (IdEquipo)+ 10)
+    From EQUIPO)
 Insert into EQUIPO values (@IdEquipo,@NomEquipo,@PaisEquipo)
-go
 
-EXEC InsertarEquipo @IDEQUIPO = 10, @NOMEQUIPO = 'test', @PAISEQUIPO = 'test'
-GO
+--EXEC InsertarEquipo @IDEQUIPO = 10, @NOMEQUIPO = 'test', @PAISEQUIPO = 'test'
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarEquipo
@@ -199,8 +226,8 @@ AS
 DELETE FROM EQUIPO WHERE IdEquipo = @IDEQUIPO
 GO
 
-EXEC EliminarEquipo @IDEquipo = 10
-GO
+--EXEC EliminarEquipo @IDEquipo = 10
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarEquipo
@@ -209,8 +236,8 @@ AS
 SELECT * FROM EQUIPO WHERE IdEquipo = @IdEquipo
 GO
 
-EXEC ConsultarEquipo @idequipo = 10
-GO
+--EXEC ConsultarEquipo @idequipo = 10
+--GO
 
 --Actualizar 
 CREATE PROCEDURE ActualizarEquipo
@@ -225,8 +252,8 @@ BEGIN
 END
 GO
 
-EXEC ActualizarEquipo @idequipo = 10, @paisequipo= 'USA'
-GO
+--EXEC ActualizarEquipo @idequipo = 10, @paisequipo= 'USA'
+--GO
 
 --5. Jugador
 --Listar
@@ -235,18 +262,25 @@ AS
 SELECT * FROM JUGADOR
 GO
 
-EXEC ListarJugador
-GO
+--EXEC ListarJugador
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarJugador
-@IdJugador int, @ApeJugador varchar(30), @NomJugador varchar(30),@NickJugador varchar(30),@EdadJugador int, @PaisJugador varchar(30),@CapJugador char(1),@IdEquipo int
+@ApeJugador varchar(30), @NomJugador varchar(30),@NickJugador varchar(30),@EdadJugador int, @PaisJugador varchar(30),@CapJugador char(1),@IdEquipo int
 AS
+declare @IdJugador int
+set @IdJugador=(Select count(*) from JUGADOR)
+if @IdJugador=0 
+       set @IdJugador = 1000
+else
+        set @IdJugador=(Select (Max (IdJugador)+ 1)
+    From JUGADOR)
 Insert into JUGADOR values (@IdJugador,@ApeJugador,@NomJugador,@NickJugador,@EdadJugador,@PaisJugador,@CapJugador,@IdEquipo)
 go
 
-EXEC InsertarJugador @idjugador = 10, @ApeJugador ='adfa',@NomJugador = 'fdaa', @NickJugador = 'nick', @EdadJugador = 20, @PaisJugador = 'asdf', @CapJugador = 'X', @idequipo = 1000
-GO
+--EXEC InsertarJugador @idjugador = 10, @ApeJugador ='adfa',@NomJugador = 'fdaa', @NickJugador = 'nick', @EdadJugador = 20, @PaisJugador = 'asdf', @CapJugador = 'X', @idequipo = 1000
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarJugador
@@ -255,8 +289,8 @@ AS
 DELETE FROM JUGADOR WHERE IdJugador = @IdJugador
 GO
 
-EXEC EliminarJugador @IDJUGADOR = 10
-GO
+--EXEC EliminarJugador @IDJUGADOR = 10
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarJugador
@@ -265,8 +299,8 @@ AS
 SELECT * FROM JUGADOR WHERE IdJugador = @IdJugador
 GO
 
-EXEC ConsultarJugador  @IdJugador = 10
-GO
+--EXEC ConsultarJugador  @IdJugador = 10
+--GO
 
 --Actualizar
 CREATE PROCEDURE ActualizarJugador
@@ -286,8 +320,8 @@ BEGIN
 END
 GO
 
-EXEC ActualizarJugador @IDJUGADOR= 10, @NICKJUGADOR= 'ASDFASDFA'
-GO
+--EXEC ActualizarJugador @IDJUGADOR= 10, @NICKJUGADOR= 'ASDFASDFA'
+--GO
 
 --6. Detalle_Personal
 --Listar
@@ -296,8 +330,8 @@ AS
 SELECT * FROM DETALLE_PERSONAL
 GO
 
-EXEC ListarDetalle_Personal
-GO
+--EXEC ListarDetalle_Personal
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarDetalle_Personal
@@ -306,8 +340,8 @@ AS
 Insert into DETALLE_PERSONAL values (@IdPersonal,@IdTorneo,@FechaInicioContrato,@FechaFinContrato)
 go
 
-EXEC InsertarDetalle_Personal @IdPersonal = 1, @IdTorneo = 10,@FechaInicioContrato = '20190101', @FechaFinContrato = '20190107'
-GO
+--EXEC InsertarDetalle_Personal @IdPersonal = 1, @IdTorneo = 10,@FechaInicioContrato = '20190101', @FechaFinContrato = '20190107'
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarDetalle_Personal
@@ -316,8 +350,8 @@ AS
 DELETE FROM DETALLE_PERSONAL WHERE IdPersonal = @IdPersonal AND IdTorneo = @IdTorneo
 GO
 
-EXEC EliminarDetalle_Personal @IdPersonal = 1, @IdTorneo = 10
-GO
+--EXEC EliminarDetalle_Personal @IdPersonal = 1, @IdTorneo = 10
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarDetalle_Personal
@@ -326,8 +360,8 @@ AS
 SELECT * FROM DETALLE_PERSONAL WHERE IdPersonal = @IdPersonal AND IdTorneo = @IdTorneo
 GO
 
-EXEC ConsultarDetalle_Personal  @IdPersonal = 1,@IdTorneo = 10 
-GO
+--EXEC ConsultarDetalle_Personal  @IdPersonal = 1,@IdTorneo = 10 
+--GO
 
 --Actualizar
 CREATE PROCEDURE ActualizarDetalle_Personal
@@ -342,8 +376,8 @@ BEGIN
 END
 GO
 
-EXEC ActualizarDetalle_Personal @IdPersonal = 1, @IdTorneo= 10,@FechaFinContrato = '20190109' 
-GO
+--EXEC ActualizarDetalle_Personal @IdPersonal = 1, @IdTorneo= 10,@FechaFinContrato = '20190109' 
+--GO
 
 --7. Detalle_Equipo 
 --Listar
@@ -352,8 +386,8 @@ AS
 SELECT * FROM DETALLE_EQUIPO
 GO
 
-EXEC ListarDetalle_Equipo
-GO
+--EXEC ListarDetalle_Equipo
+--GO
 
 --Insertar
 CREATE PROCEDURE InsertarDetalle_Equipo 
@@ -362,8 +396,8 @@ AS
 Insert into DETALLE_EQUIPO values (@IdEquipo,@IdTorneo,@Victorias,@Derrotas,@Puesto)
 go
 
-EXEC InsertarDetalle_Equipo @IdEquipo = 10, @IdTorneo = 1,@Victorias = 1, @Derrotas = 2, @Puesto = 3
-GO
+--EXEC InsertarDetalle_Equipo @IdEquipo = 10, @IdTorneo = 1,@Victorias = 1, @Derrotas = 2, @Puesto = 3
+--GO
 
 --Eliminar
 CREATE PROCEDURE EliminarDetalle_Equipo
@@ -372,8 +406,8 @@ AS
 DELETE FROM DETALLE_EQUIPO WHERE IdEquipo = @IdEquipo AND IdTorneo = @IdTorneo
 GO
 
-EXEC EliminarDetalle_Equipo @IdEquipo = 10, @IdTorneo = 1
-GO
+--EXEC EliminarDetalle_Equipo @IdEquipo = 10, @IdTorneo = 1
+--GO
 
 --Consultar
 CREATE PROCEDURE ConsultarDetalle_Equipo
@@ -382,8 +416,8 @@ AS
 SELECT * FROM DETALLE_EQUIPO WHERE IdEquipo = @IdEquipo AND IdTorneo = @IdTorneo
 GO
 
-EXEC ConsultarDetalle_Equipo  @IdEquipo = 10,@IdTorneo = 1
-GO
+--EXEC ConsultarDetalle_Equipo  @IdEquipo = 10,@IdTorneo = 1
+--GO
 
 --Actualizar
 CREATE PROCEDURE ActualizarDetalle_Equipo 
@@ -399,5 +433,5 @@ BEGIN
 END
 GO
 
-EXEC ActualizarDetalle_Equipo @IdEquipo = 10, @IdTorneo= 1, @Victorias = 3
-GO 
+--EXEC ActualizarDetalle_Equipo @IdEquipo = 10, @IdTorneo= 1, @Victorias = 3
+--GO 
